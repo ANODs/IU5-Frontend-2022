f2a5602ac1b882f92481ca32fa940339caed930f
Tim Timoshkin <51094534+ANODs@users.noreply.github.com>
2022-04-01 12:46:37 +0300

    lab3 fixed

--- lab3/1.js
+++ lab3/1.js
@@ -5,8 +5,15 @@
  * 'я вижу солнце' -> 'Я Вижу Солнце'
  * 'я Вижу солнце' -> 'Я Вижу Солнце'
  */
-function capitalize(str) {
-    // code here
+ function capitalize(str) {
+    let words = str.split(' ')
+    for(let i = 0; i < words.length; i++) {
+        let letters = []
+        letters[i] = words[i].split('')
+        letters[i][0] = letters[i][0].toUpperCase()
+        words[i] = letters[i].join('')
+    }
+    return words.join(' ')
 }
 
 module.exports = capitalize;
